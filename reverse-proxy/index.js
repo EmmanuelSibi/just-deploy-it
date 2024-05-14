b61394c891cb1e48b3b7f226dd1cc7d4fc44248a
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2024-05-14 14:17:11 +0530

    reverse proxy prisma setup and db integration

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const httpProxy = require('http-proxy');
-require('dotenv').config();
+const express = require("express");
+const httpProxy = require("http-proxy");
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
 
 const app = express();
 
-
 const PORT = process.env.PORT || 3000;
 const proxy = httpProxy.createProxy();
+const prisma = new PrismaClient();
 
 const BASE_PATH = process.env.BASE_PATH;
 
-app.use((req, res) => {
-    const hostname = req.hostname;
-    const subdomain = hostname.split('.')[0];
+app.use(async (req, res) => {
+  const hostname = req.hostname;
+  const subdomain = hostname.split(".")[0];
 
-    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+  const project = await prisma.project.findUnique({
+    where: {
+      subDomain: subdomain,
+    },
+  });
 
-   return proxy.web(req, res, { target: resolvesTo,changeOrigin:true });
-    
+  const resolvesTo = `${BASE_PATH}/${project.id}`;
+
+  return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
 });
 
-proxy.on('proxyReq', (proxyReq, req, res) => {
-    const url = req.url;
-    if(url === '/'){
-        proxyReq.path += 'index.html'
-    }
-    return proxyReq;
+proxy.on("proxyReq", (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    proxyReq.path += "index.html";
+  }
+  return proxyReq;
 });
-
-
 
 app.listen(PORT, () => {
-    console.log(`Reverse Proxy is running on port ${PORT}`);
+  console.log(`Reverse Proxy is running on port ${PORT}`);
 });
